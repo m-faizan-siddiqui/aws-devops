9325b1314bf9bf6141a2dc79f65c03b7b25ed537
faizan <mohammedfaizansiddiqui1@gmail.com>
2025-04-22 18:40:35 +0000

    Updated tag in start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo "Pulling docker image"
-docker pull "faizan007/simple-python-flask-app"
-docker run -d -p 5000:5000 "faizan007/simple-python-flask-app"
+docker pull "faizan007/simple-python-flask-app:$tag
+docker run -d -p 5000:5000 "faizan007/simple-python-flask-app:$tag
 # Run the Docker image as a container
-echo "Python app is running as container ...."
+echo "Python app:$tag
