533350c8e93f2c5fd4d99a95d58954ef25b0f4c5
m-faizan-siddiqui <159621205+m-faizan-siddiqui@users.noreply.github.com>
2025-04-24 01:43:56 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,6 +1,7 @@
 
  # Pull the Docker image from Docker Hub
  echo "Pulling docker image"
+ echo "dckr_pat_YtTcq-CP4K98O60qOYEVKFdP_70" | docker login -u faizan0007 --password-stdin
  sudo docker pull "faizan007/simple-python-flask-app:23-04-2025_17-49-25"
  sudo docker run -d -p 5000:5000 "faizan007/simple-python-flask-app:23-04-2025_17-49-25"
  # Run the Docker image as a container
