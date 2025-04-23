e2c2112ba9100416849f746f8d4d155f9a23e9ba
m-faizan-siddiqui <159621205+m-faizan-siddiqui@users.noreply.github.com>
2025-04-24 01:00:35 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,9 +2,11 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo "Pulling docker image"
 echo "dckr_pat_YtTcq-CP4K98O60qOYEVKFdP_70" | docker login -u faizan0007 --password-stdin
-docker pull "faizan007/simple-python-flask-app:23-04-2025_17-49-25"
+#docker pull "faizan007/simple-python-flask-app:23-04-2025_17-49-25"
+IMAGE="faizan007/simple-python-flask-app:23-04-2025_17-49-25"
+echo "Pulling image: $IMAGE"
+docker pull $IMAGE
 docker run -d -p 5000:5000 "faizan007/simple-python-flask-app:23-04-2025_17-49-25"
 # Run the Docker image as a container
 echo "Python is running"
