360d5c560201d9599253ce5929fe8767a3a78d6d
faiz <faiz@gmail.com>
2025-07-03 14:40:11 +0000

    Updated with latest image tag

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,6 +1,6 @@
 
  # Pull the Docker image from Docker Hub
  echo "Pulling docker image"
- sudo docker pull "faizan0007/simple-python-flask-app:03-07-2025_14-39-03"
- sudo docker run -d -p 5000:5000 "faizan0007/simple-python-flask-app:03-07-2025_14-39-03"
+ sudo docker pull "faizan0007/simple-python-flask-app:03-07-2025_14-39-45"
+ sudo docker run -d -p 5000:5000 "faizan0007/simple-python-flask-app:03-07-2025_14-39-45"
  # Run the Docker image as a container
